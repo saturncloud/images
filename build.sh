cb289884a57db0174f1a5cd8c7bfb4c5cd7dd794
hugo <hugo@illconditioned.com>
2019-08-07 15:54:31 -0400

    fixing gpu base

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 pushd saturnbase
-docker build -t saturncloud/saturnbase:0.7.3 .
+docker build -t saturncloud/saturnbase:0.7.4 .
 popd
 pushd saturnbase-gpu
-docker build -t saturncloud/saturnbase-gpu:0.7.3 .
+docker build -t saturncloud/saturnbase-gpu:0.7.4 .
 popd
 pushd saturn
-docker build -t saturncloud/saturn:0.7.3 .
+docker build -t saturncloud/saturn:0.7.4 .
 popd
 pushd saturn-gpu
-docker build -t saturncloud/saturn-gpu:0.7.3 .
+docker build -t saturncloud/saturn-gpu:0.7.4 .
 popd
 pushd saturn-r
-docker build -t saturncloud/saturn-4:0.7.3 .
+docker build -t saturncloud/saturn-r:0.7.4 .
 popd
 # docker push saturncloud/saturn:0.7.1
 # docker push saturncloud/saturnbase:0.7.0
