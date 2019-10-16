32eec122e232c9a8c5e77b4b4694d660581eb721
hugo <hugo@illconditioned.com>
2019-10-16 14:43:58 -0700

    latest build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,15 @@
 pushd saturnbase
-docker build -t saturncloud/saturnbase:0.8.3 .
+docker build -t saturncloud/saturnbase:0.9.0 .
 popd
 pushd saturn
-docker build -t saturncloud/saturn:0.8.3 .
+docker build -t saturncloud/saturn:0.9.0 .
 popd
 pushd saturnbase-gpu
-docker build -t saturncloud/saturnbase-gpu:0.8.3 .
+docker build -t saturncloud/saturnbase-gpu:0.9.0 .
 popd
 pushd saturn-gpu
-docker build -t saturncloud/saturn-gpu:0.8.3 .
+docker build -t saturncloud/saturn-gpu:0.9.0 .
 popd
 pushd saturn-r
-docker build -t saturncloud/saturn-r:0.8.3 .
+docker build -t saturncloud/saturn-r:0.9.0 .
 popd
