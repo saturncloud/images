07102c91450a2760b30a94466a3c58eb5e90ca4e
hugo <hugo@illconditioned.com>
2019-11-11 20:07:36 -0800

    build

--- scripts/build_cpu.sh
+++ scripts/build_cpu.sh
@@ -1,8 +1,4 @@
-pushd saturnbase
-docker build -t saturncloud/saturnbase:0.9.3 .
-popd
+docker build -t saturncloud/saturnbase:0.9.3 saturnbase
 docker push saturncloud/saturnbase:0.9.3
-pushd saturn
-docker build -t saturncloud/saturn:0.9.3 .
-popd
+docker build -t saturncloud/saturn:0.9.3 saturn
 docker push saturncloud/saturn:0.9.3
