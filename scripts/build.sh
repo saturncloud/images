9f90c30aefeb83bbcae1ca38a8495cfa75c0468d
hugo <hugo@illconditioned.com>
2019-11-12 07:05:13 -0800

    bumping jsaturn

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,15 @@
 pushd saturnbase
-docker build -t saturncloud/saturnbase:0.9.3 .
+docker build -t saturncloud/saturnbase:0.9.3.1 .
 popd
 pushd saturn
-docker build -t saturncloud/saturn:0.9.3 .
+docker build -t saturncloud/saturn:0.9.3.1 .
 popd
 pushd saturnbase-gpu
-docker build -t saturncloud/saturnbase-gpu:0.9.3 .
+docker build -t saturncloud/saturnbase-gpu:0.9.3.1 .
 popd
 pushd saturn-gpu
-docker build -t saturncloud/saturn-gpu:0.9.3 .
+docker build -t saturncloud/saturn-gpu:0.9.3.1 .
 popd
 pushd saturn-r
-docker build -t saturncloud/saturn-r:0.9.3 .
+docker build -t saturncloud/saturn-r:0.9.3.1 .
 popd
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,16 +1,16 @@
-docker tag saturncloud/saturnbase:0.9.3 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase:0.9.3
-docker tag saturncloud/saturn:0.9.3 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn:0.9.3
-docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase:0.9.3
-docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn:0.9.3
-docker push saturncloud/saturnbase:0.9.3
-docker push saturncloud/saturn:0.9.3
+docker tag saturncloud/saturnbase:0.9.3.1 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase:0.9.3.1
+docker tag saturncloud/saturn:0.9.3.1 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn:0.9.3.1
+docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase:0.9.3.1
+docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn:0.9.3.1
+docker push saturncloud/saturnbase:0.9.3.1
+docker push saturncloud/saturn:0.9.3.1
 
-# docker tag saturncloud/saturn-gpu:0.9.3 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-gpu:0.9.3
-# docker tag saturncloud/saturnbase-gpu:0.9.3 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase-gpu:0.9.3
-# docker tag saturncloud/saturn-r:0.9.3 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-r:0.9.3
-# docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-gpu:0.9.3
-# docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase-gpu:0.9.3
-# docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-r:0.9.3
-# docker push saturncloud/saturn-gpu:0.9.3
-# docker push saturncloud/saturnbase-gpu:0.9.3
-# docker push saturncloud/saturn-r:0.9.3
+# docker tag saturncloud/saturn-gpu:0.9.3.1 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-gpu:0.9.3.1
+# docker tag saturncloud/saturnbase-gpu:0.9.3.1 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase-gpu:0.9.3.1
+# docker tag saturncloud/saturn-r:0.9.3.1 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-r:0.9.3.1
+# docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-gpu:0.9.3.1
+# docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturnbase-gpu:0.9.3.1
+# docker push 369111250314.dkr.ecr.us-east-2.amazonaws.com/saturn-r:0.9.3.1
+# docker push saturncloud/saturn-gpu:0.9.3.1
+# docker push saturncloud/saturnbase-gpu:0.9.3.1
+# docker push saturncloud/saturn-r:0.9.3.1
