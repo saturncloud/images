051d09684a8fbc07227a698133eeab7d9e74d747
hugo <hugo@illconditioned.com>
2019-11-24 16:35:38 -0800

    bumped version

--- scripts/build_cpu.sh
+++ scripts/build_cpu.sh
@@ -1,4 +1,4 @@
-docker build -t saturncloud/saturnbase:0.9.3.1 saturnbase
-docker push saturncloud/saturnbase:0.9.3.1
-docker build -t saturncloud/saturn:0.9.3.1 saturn
-docker push saturncloud/saturn:0.9.3.1
+docker build -t saturncloud/saturnbase:0.9.3.2 saturnbase
+docker push saturncloud/saturnbase:0.9.3.2
+docker build -t saturncloud/saturn:0.9.3.2 saturn
+docker push saturncloud/saturn:0.9.3.2
