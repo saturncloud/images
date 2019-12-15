aefacb9a68e21b34fcbd97a53c0c7b6038bbd6c3
hugo <hugo@illconditioned.com>
2019-12-15 08:41:20 -0800

    update

--- scripts/build_cpu.sh
+++ scripts/build_cpu.sh
@@ -1,4 +1,4 @@
-docker build -t saturncloud/saturnbase:0.9.3.4 saturnbase
-docker push saturncloud/saturnbase:0.9.3.4
-docker build -t saturncloud/saturn:0.9.3.4 saturn
-docker push saturncloud/saturn:0.9.3.4
+docker build -t saturncloud/saturnbase:0.9.3.5 saturnbase
+docker push saturncloud/saturnbase:0.9.3.5
+docker build -t saturncloud/saturn:0.9.3.5 saturn
+docker push saturncloud/saturn:0.9.3.5
