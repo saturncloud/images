a8115535de326eaa3dbc91ef7d6b5ae8fea93987
Andy Elkin <aelkin@gmail.com>
2019-12-27 06:06:02 -0500

    daskdev-sat made lighter + cleaning

--- daskdev-sat/build_docker_image.sh
+++ daskdev-sat/build_docker_image.sh
@@ -1,10 +1,13 @@
-#
+#!/bin/bash
+
+DASKDEV_SAT_VERSION=1.02
+echo "Building daskdev-sat version $DASKDEV_SAT_VERSION" 
 
 mkdir -p saturn/pdc/dask
 cp     ../../saturn/pdc/dask/*.py  ./saturn/pdc/dask/
 
-pause
-sudo docker build -t saturncloud/daskdev-sat .
-#sudo docker push saturncloud/daskdev-sat
+sudo docker build -t saturncloud/daskdev-sat:$DASKDEV_SAT_VERSION .
+sudo docker push saturncloud/daskdev-sat:$DASKDEV_SAT_VERSION
+#sudo docker tag saturncloud/daskdev-sat:$DASKDEV_SAT_VERSION saturncloud/daskdev-sat:latest
 
 rm -r ./saturn/
