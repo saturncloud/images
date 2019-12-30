707442374d0112d2756165d2fe0af407f4d03dcb
Andy Elkin <aelkin@gmail.com>
2019-12-30 06:01:19 -0500

    dask/daskdev version tag -> :2.8.1, not :latest daskdev-sat -> :2.8.1

--- daskdev-sat/build_docker_image.sh
+++ daskdev-sat/build_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DASKDEV_SAT_VERSION=1.02
+DASKDEV_SAT_VERSION=2.8.1
 echo "Building daskdev-sat version $DASKDEV_SAT_VERSION" 
 
 sudo docker build -t saturncloud/daskdev-sat:$DASKDEV_SAT_VERSION .
