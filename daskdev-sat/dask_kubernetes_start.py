79907f976d889fa4432289459ab812de7af2eea7
Andy Elkin <aelkin@gmail.com>
2020-01-10 05:52:37 -0500

    Fix formatting; log exceptions
    * Flake8 format
    * Combine conda statements in Dockerfile
    * dasdev-sat image tagging after dask version
    * Copy just a single .py file in Dockerfile
    * Remove unused cpu parameter for start_dask_kube_cluster

--- daskdev-sat/dask_kubernetes_start.py
+++ daskdev-sat/dask_kubernetes_start.py
@@ -1,4 +1,4 @@
-# starts infinite loop process with KubeCluster in it
+# Start infinite loop process with KubeCluster in it
 
 import asyncio
 import click
@@ -6,6 +6,7 @@ import os
 from dask_kubernetes import KubeCluster, KubeConfig, InCluster, make_pod_from_dict
 import yaml
 import logging
+
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
@@ -17,28 +18,27 @@ The worker pod yaml file must be ready at this point.
 
 async def start_dask_kube_cluster(**params):
 
-    name = params.get("name", os.environ.get("DASK_NAME", None))  ## unique name of the cluster
+    name = params.get("name", os.environ.get("DASK_NAME", None))  # unique name of the cluster
     namespace = params.get(
         "namespace", os.environ.get("DASK_NAMESPACE", "community-namespace")
-    )  ## dask namespace
+    )  # dask namespace
     worker_config = params.get("worker_config", "/etc/config/worker_spec.yaml")
     starting_workers = int(
         params.get("starting_workers", os.environ.get("DASK_STARTING_WORKERS", 2))
-    )  ## starting number of worker pods
+    )  # starting number of worker pods
     max_workers = int(
         params.get("max_workers", os.environ.get("DASK_MAX_WORKERS", 10))
-    )  ## max number of worker pods for this cluster
-    cpus_per_pod = float(
-        params.get("cpus_per_pod", os.environ.get("DASK_CPU_PER_POD", 1))
-    )  ## threads on a single pod
+    )  # max number of worker pods for this cluster
     scheduler_timeout_min = int(
         params.get("scheduler_timeout_min", os.environ.get("DASK_WORKER_TOUT", 9999))
-    )  ## time (min) the cluster stays active
+    )  # time (min) the cluster stays active
 
-    kube_config_file = params.get("kube_config_file")  # TODO: remove when local testing is done
-    assert name is not None and len(name) > 0, "Name of the cluster must be set and not empty!"
+    kube_config_file = params.get("kube_config_file")
+    if name is None or len(name) == 0:
+        log.exception("Name of the cluster must be set and not empty")
+        raise Exception("Critical error")
 
-    log.info(f"Starting KubeCluster for {name} in namespace {namespace}, params: ", params)
+    log.info(f"Starting KubeCluster for {name} in namespace {namespace}, params:\n {params} ")
     max_workers = int(max_workers)
     starting_workers = max(2, int(starting_workers))
 
@@ -50,25 +50,26 @@ async def start_dask_kube_cluster(**params):
         await kube_auth.load()  # kubernetes authentication
 
     pod_template = None
-    assert (
-        worker_config is not None
-    ), "[Dask Cluster] Critical error: could not create worker config"
+    if worker_config is None or not os.path.exists(worker_config):
+        log.exception(f"Could not create dask worker config, filename {worker_config}")
+        raise Exception("Critical error")
+
     with open(worker_config) as f:
         d = yaml.safe_load(f)
         pod_template = make_pod_from_dict(d)
 
-    log.debug(pod_template)  # a lot of interesting undocumented stuff there
+    log.debug(f"\nPod template:\n{pod_template}\n")
 
     cluster = KubeCluster(
         pod_template=pod_template,
         deploy_mode="remote",
         name=name,
         namespace=namespace,
-        scheduler_timeout="%d m" % scheduler_timeout_min,
+        scheduler_timeout=f"{scheduler_timeout_min} m",
     )
 
     if starting_workers > 2:
-        log.info("Starting scaled up to %s workers" % starting_workers)
+        log.info(f"Starting scaled up to {starting_workers} workers, max is {max_workers}")
         cluster.scale_up(starting_workers)  # specify number of nodes explicitly
 
     cluster.adapt(
@@ -77,23 +78,19 @@ async def start_dask_kube_cluster(**params):
     return cluster
 
 
-## command line interface
+# command line interface
 @click.group()
-@click.pass_context
-def cli(ctx):
-    if ctx.invoked_subcommand is None:
-        print("the command you need is:  'kube-cluster'")
+def cli():
+    pass
 
 
-"""  Starts infinite async loop for KubeCluster. 
-     It will start scheduler pod and worker pods.     
-"""
+"""  Start infinite async loop for KubeCluster. It will start scheduler pod and worker pods."""
 
 
 @cli.command()
-@click.argument("name")  ## cluster name
-@click.option("--namespace")  ## dask cluster namespace
-@click.option("--worker-config")  ## worker config filename
+@click.argument("name")  # cluster name
+@click.option("--namespace")  # dask cluster namespace
+@click.option("--worker-config")  # worker config filename
 @click.option("--starting-workers")
 @click.option("--max-workers")
 @click.option("--cpus-per-pod")
@@ -114,7 +111,7 @@ def kube_cluster(
 
     kube_config_file = "~/.kube/config" if use_kube_config else None
     if kube_config_file is not None:
-        log.info("Using kube config file:", kube_config_file)
+        log.info(f"Using kube config file: {kube_config_file}")
 
     params = {"name": name}
 
@@ -134,7 +131,7 @@ def kube_cluster(
         params["scheduler_timeout_min"] = scheduler_timeout_min
 
     result = loop.run_until_complete(start_dask_kube_cluster(**params))
-    log.info(f"\nDask cluster {name} started in namespace {namespace}")
+    log.info(f"\nDask cluster {name} in namespace {namespace}")
     log.info(f"\tworker config: {worker_config}")
     log.info(f"\tworkers: start: {starting_workers}, max: {max_workers}")
     log.info(f"\tcpus per pod {cpus_per_pod}")
