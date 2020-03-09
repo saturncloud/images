bfda96772e227fb8818293eb792b17122deea46c
hugo <hugo@illconditioned.com>
2020-03-09 14:33:52 -0700

    size imporovements

--- saturnbase/install-miniconda.bash
+++ saturnbase/install-miniconda.bash
@@ -4,15 +4,13 @@ set -ex
 
 cd $(dirname $0)
 
-MINICONDA_VERSION=4.5.11
-CONDA_VERSION=4.7.5
-# CONDA_VERSION=4.5.11
+MINICONDA_VERSION=4.7.12.1
 URL="https://repo.continuum.io/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-x86_64.sh"
 INSTALLER_PATH=/tmp/miniconda-installer.sh
 wget --quiet $URL -O ${INSTALLER_PATH}
 chmod +x ${INSTALLER_PATH}
 
-MD5SUM="e1045ee415162f944b6aebfe560b8fee"
+MD5SUM="81c773ff87af5cfac79ab862942ab6b3"
 
 if ! echo "${MD5SUM}  ${INSTALLER_PATH}" | md5sum  --quiet -c -; then
     echo "md5sum mismatch for ${INSTALLER_PATH}, exiting!"
@@ -27,4 +25,3 @@ conda config --system --add channels conda-forge
 conda config --system --add channels https://conda.saturncloud.io/pkgs
 conda config --system --set auto_update_conda false
 conda config --system --set show_channel_urls true
-conda install -yq conda==${CONDA_VERSION}
