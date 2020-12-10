e051ef3a43b4cee6d78d97256ecb12fbb0f79600
James Lamb <jaylamb20@gmail.com>
2020-12-10 09:53:54 -0600

    remove templates

--- templates/saturn/install-kernel.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-conda env update -n saturn -f environment.yml
-echo '' > ${CONDA_DIR}/conda-meta/history
-${CONDA_DIR}/bin/conda clean --yes --all
--- templates/scripts/build_cpu.sh
+++
@@ -1,4 +0,0 @@
-docker build -t saturncloud/saturnbase:{{base_image_version}} saturnbase
-docker push saturncloud/saturnbase:{{base_image_version}}
-docker build -t saturncloud/saturn:{{base_image_version}} saturn
-docker push saturncloud/saturn:{{base_image_version}}
