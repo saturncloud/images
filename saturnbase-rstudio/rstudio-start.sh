0f3e00651deb4063bb7d8d3b395864887aed7369
Jacqueline Nolis <jnolis@users.noreply.github.com>
2021-12-22 14:05:29 -0800

    improved env write

--- saturnbase-rstudio/rstudio-start.sh
+++ saturnbase-rstudio/rstudio-start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "Set up environment variables"
-while IFS='=' read -r -d '' n v; do
-    printf "%s=%s\n" "$n" "$v" | sudo tee -a /usr/lib/R/etc/Renviron >/dev/null
-done < <(env -0)
+env | sudo tee -a /usr/lib/R/etc/Renviron > /dev/null
 
 echo "RStudio Server is starting"
 rstudio-server start
