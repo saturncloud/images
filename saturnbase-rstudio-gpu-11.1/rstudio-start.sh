933ec414e5d4b45dba7234c16d423d03acd24235
Jacqueline Nolis <jnolis@users.noreply.github.com>
2021-12-22 13:52:16 -0800

    added credentials fixes

--- saturnbase-rstudio-gpu-11.1/rstudio-start.sh
+++ saturnbase-rstudio-gpu-11.1/rstudio-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "Set up environment variables"
+while IFS='=' read -r -d '' n v; do
+    printf "%s=%s\n" "$n" "$v" | sudo tee -a /usr/lib/R/etc/Renviron >/dev/null
+done < <(env -0)
+
 echo "RStudio Server is starting"
 rstudio-server start
