696233705556c11a9bbf56d9ab8e40805ad3950e
Hugo <hugo@saturncloud.io>
2022-08-09 21:28:58 +0000

    julia images

--- saturnbase-julia/install-jupyter.bash
+++ saturnbase-julia/install-jupyter.bash
@@ -7,7 +7,8 @@ cd $(dirname $0)
 
 echo "installing root env:"
 cat /tmp/environment.yml
-conda install -c conda-forge mamba
+conda install conda=4.13
+conda install -c conda-forge mamba=0.25
 mamba env update -n root  -f /tmp/environment.yml
 
 conda clean -afy
@@ -17,7 +18,6 @@ npm cache clean --force
 find ${CONDA_DIR}/ -type f,l -name '*.pyc' -delete
 find ${CONDA_DIR}/ -type f,l -name '*.a' -delete
 find ${CONDA_DIR}/ -type f,l -name '*.js.map' -delete
-
 rm -rf $HOME/.node-gyp
 rm -rf $HOME/.local
 
