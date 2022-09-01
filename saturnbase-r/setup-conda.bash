e6d585a8a6300d530f27befe438336e76b6242fd
hugo <hugo@saturncloud.io>
2022-09-01 08:50:25 +0100

    More image cleanup
    - removed some extraneous differences between different R images
    - stopped R images from installing environment.yml, since all those packages (python/pip) are redundant
    - modified install-miniconda.bash to install conda/mamba before cleanup. Also forced channels.

--- saturnbase-r/setup-conda.bash
+++ saturnbase-r/setup-conda.bash
@@ -5,12 +5,6 @@ export PATH="${CONDA_BIN}:${PATH}"
 
 cd $(dirname $0)
 
-echo "installing root env:"
-cat /tmp/environment.yml
-conda install conda=4.13
-conda install -c conda-forge mamba=0.25
-mamba env update -n root  -f /tmp/environment.yml
-
 conda clean -afy
 find ${CONDA_DIR}/ -type f,l -name '*.pyc' -delete
 find ${CONDA_DIR}/ -type f,l -name '*.a' -delete
