cc250824dbd4f8b15b1592b569b70230af3479f8
hugo <hugo@saturncloud.io>
2022-11-04 09:00:23 -0700

    new cuda 11.1 python images

---
+++ saturnbase-python-gpu-11.1/install-miniconda.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /tmp
+
+set -x && \
+    MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-${CONDA_VERSION}-Linux-x86_64.sh"; \
+    SHA256SUM="4ee9c3aa53329cd7a63b49877c0babb49b19b7e5af29807b793a76bdb1d362b4"; \
+    wget "${MINICONDA_URL}" -O miniconda.sh -q && \
+    echo "${SHA256SUM} miniconda.sh" > shasum && \
+    if [ "${CONDA_VERSION}" != "latest" ]; then sha256sum --check --status shasum; fi && \
+    mkdir -p /opt && \
+    sh miniconda.sh -b -p /opt/saturncloud && \
+    rm miniconda.sh shasum && \
+    /opt/saturncloud/bin/conda install -c conda-forge main::conda=4.13 conda-forge::mamba=0.25 && \
+    ln -s /opt/saturncloud/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/saturncloud/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc && \
+    find /opt/saturncloud/ -follow -type f -name '*.a' -delete && \
+    find /opt/saturncloud/ -follow -type f -name '*.js.map' -delete && \
+    /opt/saturncloud/bin/conda clean -afy && \
+    chown -R 1000:1000 /opt/saturncloud
