de301818e54f118cbaae57ebfbf1a2cee89f2d21
hugo <hugo@saturncloud.io>
2023-10-20 19:30:06 +0000

    standardizing jupyter envs

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -8,3 +8,11 @@ cp ./common/install-miniconda.bash ./saturnbase-r-gpu/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-r-bioconductor/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-julia-gpu-11.8/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-julia/install-miniconda.bash
+
+cp ./common/jupyter-environment.yml ./saturnbase-python/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-12.1/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-devel-12.1/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-devel-11.8/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-11.8/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-julia-gpu-11.8/environment.yml
+cp ./common/jupyter-environment.yml ./saturnbase-julia/environment.yml
