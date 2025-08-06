8facc4c0303a630d7cca4bd10f39e6a3ec5ce261
hugo <hugo@saturncloud.io>
2025-08-06 13:31:37 -0700

    cuda 12.9

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,8 +1,8 @@
 cp ./common/install-miniconda.bash ./saturnbase-python/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-python-gpu-12.1/install-miniconda.bash
-cp ./common/install-miniconda.bash ./saturnbase-python-gpu-12.4/install-miniconda.bash
+cp ./common/install-miniconda.bash ./saturnbase-python-gpu-12.9/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-python-gpu-devel-12.1/install-miniconda.bash
-cp ./common/install-miniconda.bash ./saturnbase-python-gpu-devel-12.4/install-miniconda.bash
+cp ./common/install-miniconda.bash ./saturnbase-python-gpu-devel-12.9/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-python-gpu-devel-11.8/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-python-gpu-11.8/install-miniconda.bash
 cp ./common/install-miniconda.bash ./saturnbase-r/install-miniconda.bash
@@ -11,17 +11,17 @@ cp ./common/install-miniconda.bash ./saturnbase-r-bioconductor/install-miniconda
 
 cp ./common/jupyter-environment.yml ./saturnbase-python/environment.yml
 cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-12.1/environment.yml
-cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-12.4/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-12.9/environment.yml
 cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-devel-12.1/environment.yml
-cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-devel-12.4/environment.yml
+cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-devel-12.9/environment.yml
 cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-devel-11.8/environment.yml
 cp ./common/jupyter-gpu-environment.yml ./saturnbase-python-gpu-11.8/environment.yml
 
 
 cp ./common/install-jupyter.bash ./saturnbase-python-gpu-devel-12.1/install-jupyter.bash
-cp ./common/install-jupyter.bash ./saturnbase-python-gpu-devel-12.4/install-jupyter.bash
+cp ./common/install-jupyter.bash ./saturnbase-python-gpu-devel-12.9/install-jupyter.bash
 cp ./common/install-jupyter.bash ./saturnbase-python/install-jupyter.bash
 cp ./common/install-jupyter.bash ./saturnbase-python-gpu-12.1/install-jupyter.bash
-cp ./common/install-jupyter.bash ./saturnbase-python-gpu-12.4/install-jupyter.bash
+cp ./common/install-jupyter.bash ./saturnbase-python-gpu-12.9/install-jupyter.bash
 cp ./common/install-jupyter.bash ./saturnbase-python-gpu-11.8/install-jupyter.bash
 cp ./common/install-jupyter.bash ./saturnbase-python-gpu-devel-11.8/install-jupyter.bash
